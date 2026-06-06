1ebe58e866c06243f48479e8bcc296430fd03721
Siddharth <siddharth140705@gmail.com>
2026-06-06 18:54:10 +0530

    error check in ui

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ if video_id and query:
                     st.error("Unable to fetch transcript.")
                     st.info("Try another video.")
                 else:
-                    st.error(" Unexpected error occurred.")
+                    st.error(str(e))
 
                 st.stop()
     
